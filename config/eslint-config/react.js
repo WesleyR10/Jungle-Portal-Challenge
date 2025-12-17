5c7265535cb4cfbabdac933f4f1ecbe25681461d
WesleyR10 <wesleyribas2015@gmail.com>
2025-12-17 15:46:29 -0300

    chore(eslint-config): tune react linting rules and patterns

--- config/eslint-config/react.js
+++ config/eslint-config/react.js
@@ -3,10 +3,7 @@ module.exports = {
   extends: [
     "eslint:recommended",
     "plugin:@typescript-eslint/recommended",
-    "plugin:react/recommended",
-    "plugin:react-hooks/recommended",
     "plugin:prettier/recommended",
-    "eslint-config-turbo"
   ],
   plugins: ["@typescript-eslint", "simple-import-sort"],
   globals: {
@@ -21,12 +18,12 @@ module.exports = {
       version: "detect",
     },
   },
-  ignorePatterns: [
-    ".*.js",
-    "node_modules/",
-    "dist/",
-  ],
+  ignorePatterns: [".*.js", "**/*.cjs", "node_modules/", "dist/"],
   rules: {
+    "@typescript-eslint/no-explicit-any": "warn",
+    "@typescript-eslint/no-unused-vars": "warn",
+    "@typescript-eslint/no-require-imports": "off",
+    "no-empty": "warn",
     "react/react-in-jsx-scope": "off",
     "simple-import-sort/imports": "error",
     "simple-import-sort/exports": "error",
